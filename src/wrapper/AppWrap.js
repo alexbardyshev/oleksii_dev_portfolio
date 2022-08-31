18ff9a9a518ec129af5bd4f4d2d3ef37d6c50d9e
Oleksìi <bardyshev@gmail.com>
2022-08-31 21:52:27 +0300

    Work section was created

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -12,8 +12,8 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
                 <Component />
 
                 <div className="copyright">
-                    <p className="p-text">@2022 Oleksii</p>
-                    <p className="p-text">All rights reserved</p>
+                    <p className="p-text">© 2022 Oleksii Dev</p>
+                    <p className="p-text">All rights reserved®</p>
                 </div>
             </div>
             <NavigationDots active={idName} />
