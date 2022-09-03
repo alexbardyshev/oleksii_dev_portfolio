68cfd7e24fc741c73a6152836c05621f5ab44b41
Oleksìi <bardyshev@gmail.com>
2022-09-03 10:14:20 +0300

    Testimonial section was added

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
-import { AppWrap, MotionWrap } from '../../wrapper';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
+import { MotionWrap } from '../../wrapper/MotionWrap';
 
 const Skills = () => {
     const [experiences, setExperiences] = useState([]);
@@ -88,4 +89,8 @@ const Skills = () => {
     );
 };
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+    MotionWrap(Skills, 'app__skills'),
+    'skills',
+    'app__whitebg',
+);
